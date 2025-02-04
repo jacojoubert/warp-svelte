dc8b9c45de5ceb2585cf42ad69030e24509c7d26
Jaco Joubert <jaco@hey.com>
2025-02-04 09:08:13 -0500

    Add request manager
    Can't import from @ember-data/request

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import manager from '../utils/request-manager';
+	import { browser } from '$app/environment';
+
+	const fetchData = async function () {
+		if (!browser) {
+			return;
+		}
+		const response = await manager.request({
+			url: `/users`
+		});
+		debugger;
+	};
+</script>
+
+<h1>Warp Drive Testing App</h1>
+<button onclick={fetchData}>Fetch Data</button>
---
+++ src/utils/request-manager.ts
@@ -0,0 +1,10 @@
+// TODO can't import from '@ember-data/request' package
+// [vite] Error when evaluating SSR module /src/utils/request-manager.ts: failed to import "@ember-data/request"
+// |- Error: this method is really implemented at compile time via a babel plugin. If you're seeing this exception, something went wrong
+// at getGlobalConfig
+import RequestManager from '@ember-data/request';
+import Fetch from '@ember-data/request/fetch';
+
+const manager = new RequestManager().use([Fetch]);
+
+export default manager;
