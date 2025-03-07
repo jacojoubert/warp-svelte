d0141f152e2b1f2f66d9100a6ae4181b42ed9dc3
Jaco Joubert <jaco@hey.com>
2025-03-07 15:29:30 -0500

    WIP

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
+import { buildMacros } from '@embroider/macros/babel';
+console.log(buildMacros);
+
+const builtMacros = buildMacros({
+	// this is how you configure your own package
+	setOwnConfig: {
+		// your config goes here
+	},
+	// this is how you can optionally send configuration into your
+	// dependencies, if those dependencies choose to use
+	// @embroider/macros configs.
+	setConfig: {}
+});
+
 export default function (api) {
 	api.cache(true);
 
@@ -19,7 +33,8 @@ export default function (api) {
 					}
 				},
 				'ember-data-specific-macros-stripping'
-			]
+			],
+			...builtMacros.babelMacros
 		]
 	};
 }
