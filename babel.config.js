797af3909d1b2a62841b44b028e27e507f6b4b72
Jaco Joubert <jaco@hey.com>
2025-04-07 09:25:14 -0400

    Embroider (#1)
    * WIP
    
    * WIP
    
    * Update babel.config.js
    
    * Upgrade deps

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
+import { buildMacros } from '@embroider/macros/babel';
+import { setConfig } from '@warp-drive/build-config';
+
+const builtMacros = buildMacros({
+	configure(macrosInstance) {
+		setConfig(macrosInstance, './', { ___legacy_support: false });
+	},
+
+	// this is how you configure your own package
+	setOwnConfig: {
+		// your config goes here
+	},
+	// this is how you can optionally send configuration into your
+	// dependencies, if those dependencies choose to use
+	// @embroider/macros configs.
+	setConfig: {}
+});
+
 export default function (api) {
 	api.cache(true);
 
@@ -19,7 +37,8 @@ export default function (api) {
 					}
 				},
 				'ember-data-specific-macros-stripping'
-			]
+			],
+			...builtMacros.babelMacros
 		]
 	};
 }
