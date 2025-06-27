32a3ff834fc3f91c3b56926c9fbdc9ea07413306
Jaco Joubert <jaco@hey.com>
2025-06-27 10:58:46 -0400

    Add logging

--- src/lib/utils/signals.ts
+++ src/lib/utils/signals.ts
@@ -1,16 +1,19 @@
 import * as $ from 'svelte/internal/client';
 
 const createSignal = (obj: object, key: string | symbol) => {
+	console.log('createSignal', obj, key);
 	const signal = $.state(obj);
 	return signal;
 };
 
 const consumeSignal = (signal) => {
+	console.log('consumeSignal', signal);
 	return $.get(signal);
 };
 
 const notifySignal = (signal) => {
-	return $.set(signal, $.get(signal), true);
+	console.log('notifySignal', signal);
+	return $.set(signal, $.get(signal) + 1, true);
 };
 
 const buildSignalConfig = () => {
